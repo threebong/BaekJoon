ab8f63dcfabd67ffa261d894668cbc3c38bcba0e
threebong <88201568+threebong@users.noreply.github.com>
2022-09-19 17:40:20 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 28 ms, Memory: 422800 KB -BaekjoonHub

--- 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
@@ -1,9 +1,11 @@
 public class Test {
     long sum(int[] a) {
+        int size = a.length;
         long ans = 0;
-        for(int i=0;i<a.length;i++){
+        for(int i=0; i<size; i++){
             ans += a[i];
         }
+        
         return ans;
     }
 }
